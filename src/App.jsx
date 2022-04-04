5915cb0db6c358d5c4cd376fc405357b7ba59c0e
Vijendra <docs.vijendra@gmail.com>
2022-04-04 16:41:28 +0530

    css added for p-left and p-right

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import './chat.css'
 import { io } from 'socket.io-client'
-import { useState } from 'react'
-// const socket = io('http://localhost:7000')
+import { useEffect, useRef, useState } from 'react'
+import { EmojiPicker, Emoji } from "react-emoji-search";
+
+
+
+
 const socket = io('https://ervijuchat.herokuapp.com')
 const joined_user = prompt('Enter your name')
+console.log({joined_user})
 socket.emit('new-user-joined', joined_user)
+
+
+
 function App() {
   const [message, setMessage] = useState("")
   const [chats, setchats] = useState([])
-  const [users, setUsers] = useState([])
   const sendMessage = (e) => {
     e.preventDefault()
     socket.emit('send', message)
@@ -24,24 +31,33 @@ function App() {
   socket.on('allusers', (users) => {
     setUsers(users)
   })
-  // socket.on('get-typing',(user)=>{
 
-  // })  
 
+  const messageEl = useRef(null)
+  useEffect(()=>{
+    if(messageEl){
+      messageEl.current.addEventListener('DOMNodeInserted', event=>{
+        const { currentTarget : target} = event
+        target.scroll({ top : target.scrollHeight, behaviour : 'smooth'})
+      })
+    }
+  })
   return (
     <div className="App">
       <h1 className='title'>ERchat</h1>
       <div className='container'>
-        <div className='chatsContainer'>
+      <div className='useralign'>
+      <span className='user'>You have joined as {joined_user}</span>
+      </div>
+        <div className='chatsContainer' ref={messageEl}>
           {chats.map(({ position, user, message }, index) => {
             if (position == 'left')
-              return <p key={index} className='left message'>{user} : {message}</p>
+              return <p key={index} className='left message'><span className='username'>{user}</span> : {message}</p>
             else if (position == 'right')
-              return <p key={index} className='right message'>{user} : {message}</p>
+              return <p key={index} className='right message'><span className='username'>{user}</span> : {message}</p>
             else
               return <h5 key={index} className='mid message'>{message}</h5>
           })}
-        {/* <p key={index} className='left message typing'>{user} : ...typing</p> */}
         </div>
         <form >
           <input onChange={(e) => {
